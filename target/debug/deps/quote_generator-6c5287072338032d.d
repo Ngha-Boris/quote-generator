73f1ae51183990230dabf7a76a5a68e2134aab96
Ngha-Boris <borisngha6@gmail.com>
2026-05-26 17:09:45 +0100

    add docker build job to the ci

---
+++ target/debug/deps/quote_generator-6c5287072338032d.d
@@ -0,0 +1,5 @@
+/home/boris/personal_test_projecrs/quote-generator/target/debug/deps/quote_generator-6c5287072338032d.d: src/main.rs
+
+/home/boris/personal_test_projecrs/quote-generator/target/debug/deps/libquote_generator-6c5287072338032d.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/quote_generator-c2430722eb3fb8ea.d
@@ -0,0 +1,5 @@
+/home/boris/personal_test_projecrs/quote-generator/target/debug/deps/quote_generator-c2430722eb3fb8ea.d: src/main.rs
+
+/home/boris/personal_test_projecrs/quote-generator/target/debug/deps/libquote_generator-c2430722eb3fb8ea.rmeta: src/main.rs
+
+src/main.rs:
